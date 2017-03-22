3c6065cdbcb90f9322bd2a21565c1f6fd4fc675c
joycatzt713 <joycatzt@gmail.com>
2017-03-22 15:05:11 -0700

    update note

--- DataProcesser/test.py
+++ DataProcesser/test.py
@@ -1,13 +1,13 @@
-#import textract
+import textract
 
 def main():
 	print ("Data processor made by Tong. Start.\n")
-	#text = textract.process('test.pdf', method='pdfminer')
-	text = "bowen tian \n 1234 abcd way, irvine, ca 900000\n Data Engineer"
+	text = textract.process('test.pdf')
+#	text = "bowen tian \n 1234 abcd way, irvine, ca 900000\n Data Engineer"
 	print (text)
-	for line in text:
-		for word in line.split(" "):
-			print (word)
+#	for line in text:
+#		for word in line.split(" "):
+#			print (word)
 
 if __name__ == '__main__':
 	main()
