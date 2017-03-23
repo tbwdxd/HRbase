4ee41571e8b83d8fe52151ba6b6955d8003a3db4
HaoranMa <14102512@163.com>
2017-03-23 15:32:14 -0700

    Updated Database and Test

--- DataBaseCreating/CreateDataBase.sql
+++ DataBaseCreating/CreateDataBase.sql
@@ -1,18 +1,29 @@
-# Date: 2017年3月22日
-# Author： 马浩然
-# Description： 创建三个表，分别是Talent， Company， Employment_History
+# Updated on Mar. 23th, 2017
+# Author： Haoran Ma
+# Description： Created three tables, 'Talent'， 'Company'， 'Employment_History'
 
+#1 
 CREATE DATABASE HRbase;
 
+#2 Select Database
 USE HRbase;
 
+#3 Create Table Talent, Company, Employment_History
+#3.1 In table Talent, the primary Key is 'PersonID' and 'EMail' 
 CREATE TABLE Talent (PersonID INT NOT NULL AUTO_INCREMENT, LastName VARCHAR(100) NOT NULL, FirstName VARCHAR(100) NOT NULL,
 Age INT, LivingArea VARCHAR(100), CellPhone VARCHAR(25), EMail VARCHAR(100) NOT NULL, Major VARCHAR(100), Degree VARCHAR(100),
-GraduteSchool VARCHAR(255), Citizen VARCHAR(25), Source VARCHAR(50), LinkedIn VARCHAR(255), ResumeLocation VARCHAR(255),
-PRIMARY KEY(PersonID), UNIQUE "EMail" ("EMail"));
+GraduteSchool VARCHAR(255), Citizen VARCHAR(25), SubmissionDate Date, Source VARCHAR(50), LinkedIn VARCHAR(255), ResumeLocation VARCHAR(255), 
+Note VARCHAR(255), PRIMARY KEY(PersonID,EMail));
 
+#3.2 In table Company, the primary key is 'CompanyID'
 CREATE TABLE Company (CompanyID INT NOT NULL, CompanyName VARCHAR(255), CompanyInf VARCHAR(255), PRIMARY KEY(CompanyID));
 
+#3.3 In table Employment_History, the primary key is 'EmploymentID', and two foreign key, 'PersonId' and 'CompanyID'
 CREATE TABLE Employment_History (EmploymentID INT NOT NULL, PersonID INT NOT NULL, CompanyID INT NOT NULL, JobField VARCHAR(100),
 Position VARCHAR(100), StartDate Date, EndDate Date, JobExp VARCHAR(255), PRIMARY KEY(EmploymentID), 
 FOREIGN KEY(PersonID) REFERENCES Talent(PersonID), FOREIGN KEY(CompanyID) REFERENCES Company(CompanyID));
+
+#If you want to drop the Table, please follow the order:
+DROP Table Employment_History;
+DROP TABLE Company;
+DROP TABLE Talent;
--- DataBaseCreating/Test1.sql
+++ DataBaseCreating/Test1.sql
@@ -3,12 +3,13 @@ USE HRbase;
 # Insert Value in 'Talent' table
 INSERT INTO Talent 
 (LastName, FirstName, Age, LivingArea, CellPhone, EMail, Major, Degree, GraduteSchool, 
-Citizen, Source, LinkedIn, ResumeLocation) 
+Citizen, SubmissionDate, Source, LinkedIn, ResumeLocation, Note) 
 VALUES 
-("Ma", "Haoran", 25, "California", "626-478-5299","haoranma0101@gmail.com", "Electrical Engineering", "PhD", "Standford", "Yes", "", "",""),
-("Tian", "Bowen", 27, "California", "","tianbowen@gmail.com", "Computer Science","PhD", "Harvard", "Yes", "", "",""),
-("Zhang", "Tong", 25, "California", "","zhangtong@gmail.com", "Electrical Engineering","PhD", "MIT", "Yes", "", "",""),
-("Cheng", "Songda", 24, "California", "","chengsongda@gmail.com", "Chemistry","PhD", "UCB", "Yes", "", "","");
+("Ma", "Haoran", 25, "California", "626-478-5299","haoranma0101@gmail.com", "Electrical Engineering", "PhD", "Standford", "Yes", 
+	"2016-6-1", "","","","Good"),
+("Tian", "Bowen", 27, "California", "","tianbowen@gmail.com", "Computer Science","PhD", "Harvard", "Yes", "2016-6-1", "","","","Good"),
+("Zhang", "Tong", 25, "California", "","zhangtong@gmail.com", "Electrical Engineering","PhD", "MIT", "Yes", "2016-6-1", "","","","Good"),
+("Cheng", "Songda", 24, "California", "","chengsongda@gmail.com", "Chemistry","PhD", "UCB", "Yes", "2016-6-1", "","","","Good");
 # In table 'talent', there is one problem need to be concerned. We cannot distinguish duplicate since no unique information.
 # I try to use EMail as UniquKey but system treat null information as duplicate. Therefore, we need to control duplication
 # somewhere in the system later.
@@ -46,3 +47,12 @@ from Employment_History
 inner join Talent on Talent.PersonID = Employment_History.PersonID
 inner join Company on Company.CompanyID = Employment_History.CompanyID
 where Talent.FirstName = 'Haoran';
+
+
+# SD require to test:
+# Informaiton shows In this order: major, highest degree, latest graduated from.
+Select FirstName, LastName, Major, Degree, GraduteSchool from Talent;
+
+# Information shows current job title
+Select Talent.FirstName, Talent.LastName, Employment_History.Position, Employment_History.JobField, Employment_History.StartDate,
+Employment_History.EndDate from Employment_History inner join Talent on Employment_History.PersonID = Talent.PersonID;
