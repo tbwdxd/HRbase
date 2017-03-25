00a7af0c31b3a248d277dee302f9877d0d00b1fa
joycatzt713 <joycatzt@gmail.com>
2017-03-24 18:13:31 -0700

    add comment

--- Program/testWordMatching.py
+++ Program/testWordMatching.py
@@ -2,21 +2,18 @@ import textract
 import re
 def main():
 
-#split the original resume by space
+	#split the original resume by space
 	text = textract.process('testDoc.doc')
 	finaltext = text.decode('UTF-8')
 	splitext = re.findall(r"[\w']+", finaltext)	#split is a list
 	dict_ = ["software engineer","Java","Electrical Engineering","HTML","System Engineer","Mechanical Engineering","Python","DOORS","team player","Chemistry","Tomatoes","League of Legends"]
-	#dict_ = ["Tong","Software Engineer"]
 	#count store the matching words and their matching times
-	result = {}									#result is a dictionary
-		#calculate number of matching word in the original text
-		#do nothing
+	result = {}													   #result is a dictionary
+	#calculate number of matching word in the original text
 	index_i = -1
 	for element in splitext:
 		index_i += 1
 		compare(element, dict_, splitext,index_i,result)
-			#update result[keyword]
 	print('Matched keywords are as follows: ')
 	for key in result:
 		print ("Word: ",key, " Count: ",result[key])
@@ -25,57 +22,26 @@ def compare(element, dict_, splitext,index_i,result):             #check if elem
 	for keyword in dict_:
 		newkeyword = keyword.split()
 		if(len(newkeyword) > 1):               					  #if current keyword is more than one word
-			if element == newkeyword[0]:
+			if element == newkeyword[0]:						  #if the first word of the key word matched
 				tempindex = index_i
 				is_match = True
-				for i in range(1, len(newkeyword)):
+				for i in range(1, len(newkeyword)):				  #continue match the rest of the keyword
 					tempindex += 1
-					if (splitext[tempindex] != newkeyword[i]):
+					if (splitext[tempindex] != newkeyword[i]):    #if the one of the word in the key word doesn't match
 						is_match = False
 						break
-				if is_match == True:
+				if is_match == True:    						  #if the keyowrd totally matched, add the matching keyword in the result dictionary
 					if keyword not in result:
 						result[keyword] = 1
 					else:
 						result[keyword] += 1
-		else:
+		else:													 #if current keyword is a single word
 			if newkeyword[0] == element:
 				if newkeyword[0] not in result:
 					result[newkeyword[0]] = 1
 				else:
 					result[newkeyword[0]] += 1
 	#see if match, handle cap or non cap
-
-"""
-def matchingTime(splitext, dict, count):        #
-	countime = -1 
-	matchingtime = 0
-	for element in splitext:
-		count += 1
-		for keyword in dict:
-			match = True
-			if(len(keyword) > 1):               #if current keyword is more than one word
-				keyword = keyword.split()
-				if element == keyword[0]:
-					tempindex = countime
-					for i in (0, len(keyword)):
-						tempindex += 1
-						if (splitext[tempindex] != keyword):
-							match = False
-							break
-							matchingtime += 1
-					if matchingtime == len(keyword):
-						if keyword not in count:
-							count[keyword] = 1
-						else:
-							count[keyword] += 1
-				else: 
-					match = False
-			else:								#if current keyword is single word
-				if keyword != element:
-					match = False
-				else:
-"""					
 	
 
 if __name__ == '__main__':
