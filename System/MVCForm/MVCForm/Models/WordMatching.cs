dc1c90c683a42ebe6621493f16f0b52e7f0acc2f
HaoranMa <14102512@163.com>
2017-04-21 13:15:31 -0700

    update

---
+++ System/MVCForm/MVCForm/Models/WordMatching.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace MVCForm.Models
+{
+    public class WordMatching
+    {
+        private textParser parser = new textParser();
+        private JSON Dict;
+        private string Doc;
+        public void matching(string[] args)
+        {
+            args[0] = "C:\\Users\\COMACIT\\Desktop\\HRbase\\PythonDataParser";
+            //args[1] = "";
+            String text = parser.ExtractTextFromPdf(args[0]);
+
+        }
+
+    }
+
+    
+    
+}
---
+++ System/MVCForm/MVCForm/Models/textParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace MVCForm.Models
+{
+    public class textParser
+    { 
+        public string ExtractTextFromPdf(string path)
+        {
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder text = new StringBuilder();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+
+                return text.ToString();
+            }
+        }
+
+    
+
+    }
+}
